791a25fd233c9389b8773e1a8518633bc7d85066
ankitagarwal895 <ankit.agarwal895@gmail.com>
2016-04-19 03:01:52 +0530

    Default URLs Redirecting

--- THEMATRIX/urls.py
+++ THEMATRIX/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from chat import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^chat/',include('chat.urls')),
+    url(r'^login/', views.log_in),
+    url(r'^',views.signup),
 ]
